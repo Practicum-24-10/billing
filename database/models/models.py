2970bcdb27eb2fa0872c432f08158404f4f888f8
Slender357 <114290259+Slender357@users.noreply.github.com>
2023-09-27 21:52:52 +0300

    fix db

--- database/models/models.py
+++ database/models/models.py
@@ -52,13 +52,16 @@ class Subscription(Base):
     duration: Mapped[int] = mapped_column(Integer, nullable=False)
     amount: Mapped[float] = mapped_column(Float, nullable=False)
     title: Mapped[str] = mapped_column(String, nullable=False)
+    currency: Mapped[str] = mapped_column(String, nullable=False)
     active: Mapped[bool] = mapped_column(Boolean, nullable=False)
-    role_id: Mapped[UUID] = mapped_column(_UUID, nullable=False)
+    permission: Mapped[str] = mapped_column(String, nullable=False)
     users_subscriptions: Mapped[list["UsersSubscriptions"]] = relationship(
-        "UsersSubscriptions", back_populates="subscription"
+        "UsersSubscriptions", back_populates="subscription",
+        foreign_keys="[UsersSubscriptions.subscription_id]",
     )
     users_next_subscriptions: Mapped[list["UsersSubscriptions"]] = relationship(
-        "UsersSubscriptions", back_populates="next_subscription"
+        "UsersSubscriptions", back_populates="next_subscription",
+        foreign_keys="[UsersSubscriptions.next_subscription_id]",
     )
 
 
@@ -92,12 +95,12 @@ class UsersSubscriptions(Base):
         ForeignKey("subscription.id"), nullable=False
     )
 
-    subscription: Mapped["Subscription"] = relationship(
-        back_populates="users_subscriptions"
-    )
-    next_subscription: Mapped["Subscription"] = relationship(
-        back_populates="users_next_subscriptions"
-    )
+    subscription: Mapped["Subscription"] = relationship("Subscription",
+                                                        back_populates="users_subscriptions",foreign_keys=[subscription_id],overlaps="subscription,users_subscriptions"
+                                                        )
+    next_subscription: Mapped["Subscription"] = relationship("Subscription",
+                                                             back_populates="users_next_subscriptions",foreign_keys=[subscription_id],overlaps="subscription,users_subscriptions"
+                                                             )
 
     start_at: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
     expires_at: Mapped[datetime] = mapped_column(TIMESTAMP, nullable=False)
