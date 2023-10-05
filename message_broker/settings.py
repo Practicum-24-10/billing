caed1e419103d55ecbba033bcbb1b3d8c06fedfd
Сергей К <medium.well@ya.ru>
2023-10-05 23:25:00 +0300

    flake8 fix

--- message_broker/settings.py
+++ message_broker/settings.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     amqp_host: str = 'localhost'
     amqp_port: int = 5672
-    
+
     def get_amqp_url(self) -> str:
         return f'amqp://{self.amqp_host}:{self.amqp_port}/'
 
