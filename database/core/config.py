ab6bb44a34ea7f1e18b194d2c0a926e143701f00
ghosteeeeee <47458761+ghosteeeeee@users.noreply.github.com>
2023-10-07 21:05:17 +0300

    Update config.py

--- database/core/config.py
+++ database/core/config.py
@@ -13,7 +13,7 @@ class PostgresConfig(MainSettings):
     db_user: str = Field("app", env="DB_USER")
     db_password: str = Field(" ", env="DB_PASSWORD")
     db_host: str = Field("localhost", env="DB_HOST")
-    db_port: str = Field("5432", env="DB_PORT"
+    db_port: str = Field("5432", env="DB_PORT")
 
 
 pg_config = PostgresConfig()
